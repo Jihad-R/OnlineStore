0d41794f72d55ed03f475beff27c9354f52b2dd6
Jihad-R <jihad.jobo@gmail.com>
2023-04-30 15:21:12 -0500

    refactored code base

--- src/main/java/org/yearup/OnlineStoreApp.java
+++ src/main/java/org/yearup/OnlineStoreApp.java
@@ -11,53 +11,67 @@ public class OnlineStoreApp {
     private HashMap<String, Product> products = new HashMap<>(); // declare the products hashmap
     private ArrayList<Product> shoppingCart = new ArrayList<>(); // declare the shopping cart
 
+    private Set<Product> distinct; // delcare a set to store all the distinct products
     String userInput; // stores the id of the product to be added to the cart
     private boolean isValidOption = false; // checks if the option selected is valid
-    private Set<Product> distinct; // delcare a set to store all the distinct products
-    private int productQuantity;
+    double sum = 0.00; // declare the variable that would store the total
 
 
-    public void run() {
-
+    public void run() //
+    {
         loadInventory();
         homeScreen();
     }
 
 
-    public void loadInventory() {
+    // Adds all the products in .csv file into a Hashmap
+    public void loadInventory()
+    {
+        try // try and catch clause to catch any exception regarding the .csv file
+        {
+            FileInputStream fileInputStream = new FileInputStream("inventory.csv"); // open file stream
+            String line;
+            scanner = new Scanner(fileInputStream); // scan the input stream
 
-        try {
-            FileInputStream fileInputStream = new FileInputStream("inventory.csv");
-            String line = null;
-            scanner = new Scanner(fileInputStream);
+            while (scanner.hasNextLine()) // loop if there is a next line in the .csv file
+            {
+                line = scanner.nextLine(); // store the content of the next line in the csv file
 
-            while (scanner.hasNextLine()) {
-
-                line = scanner.nextLine();
-                String[] productInfo = line.split("\\|");
+                String[] productInfo = line.split("\\|"); // split the string on the '|'
                 String id = productInfo[0];
                 String name = productInfo[1];
                 double price = Double.parseDouble(productInfo[2]);
 
-                products.put(id, new Product(id, name, price));
-
+                products.put(id, new Product(id, name, price)); // add a new product to the Hashmap
             }
 
-        } catch (FileNotFoundException ex) {
-            System.out.println(ConsoleColor.RED + "File not found!" + ConsoleColor.RESET);
-        } finally {
+        }
+
+        catch (FileNotFoundException ex) // catch exception if the file is not found
+        {
+            System.out.println(ConsoleColor.RED + "\nFile not found!\n" + ConsoleColor.RESET);
+        }
+
+        catch (Exception ex) // catch other exceptions
+        {
+            System.out.println(ConsoleColor.RED + "\nSomething went wrong!\n" + ConsoleColor.RESET);
+        }
+
+        finally
+        {
             scanner.close();
         }
 
 
     }
 
-    public void homeScreen() {
-
-        scanner = new Scanner(System.in);
+    public void homeScreen() //
+    {
+        scanner = new Scanner(System.in); // scan input stream from the user's keyboard
         int option; // stores the value of the option selected
 
-        do {
+        do // do-while loop that loops until a valid option is selected
+        {
             // home screen UI
             System.out.println("=========================");
             System.out.println("ONLINE STORE HOME SCREEN");
@@ -72,40 +86,44 @@ public class OnlineStoreApp {
             {
                 option = scanner.nextInt();
 
-                switch (option) {
-                    case 0:
-                        System.exit(1);
-                    case 1: {
+                switch (option) // switch cases to validate user input
+                {
+                    case 0 -> System.exit(1);
+                    case 1 ->
+                    {
                         displayProduct();
                         isValidOption = true;
-                        break;
                     }
-                    case 2: {
+                    case 2 ->
+                    {
                         scanner.nextLine();
                         displayShoppingCart();
                         isValidOption = true;
-                        break;
 
                     }
-                    default:
-                        System.out.println(ConsoleColor.RED + "The command selected is invalid!" + ConsoleColor.RESET);
+                    default ->
+                    {
+                        System.out.println(ConsoleColor.RED + "The command selected is invalid!"
+                                + ConsoleColor.RESET); // error message
                         isValidOption = false;
+                    }
                 }
-            } catch (InputMismatchException ex) {
-                System.out.println(ConsoleColor.RED + "Invalid Input! Valid inputs are integers between 0 - 2 " + ConsoleColor.RESET);
+            }
+
+            catch (InputMismatchException ex)
+            {
+                System.out.println(ConsoleColor.RED + "Invalid Input! Valid inputs are integers between 0 - 2."
+                        + ConsoleColor.RESET); // error message
 
                 homeScreen();
             }
-
         }
         while (!isValidOption);
 
     }
 
-    private void displayProduct() {
-
-
-        scanner = new Scanner(System.in);
+    private void displayProduct() // display product screen UI
+    {
         isValidOption = false;
 
         //formatting the header of the CLI table
@@ -120,74 +138,96 @@ public class OnlineStoreApp {
             System.out.println("------- ----------------------------------- ------");
         }
 
-        do {
-
+        do // do-while loop that loops until an input is valid
+        {
             System.out.println("Would you like to add a product to your shopping cart?");
             System.out.println("- Enter the ID of the product to add to cart");
             System.out.println("- Press 'X' for home screen");
+
             System.out.print("Product ID: ");
             userInput = scanner.nextLine().toUpperCase().trim();
 
-            if (products.containsKey(userInput)) {
-
-                shoppingCart.add(products.get(userInput));
+            if (products.containsKey(userInput))
+            {
                 isValidOption = true;
-                homeScreen();
-            } else {
-                System.out.println(ConsoleColor.RED + "Invalid Input! Please try again" + ConsoleColor.RESET);
+                shoppingCart.add(products.get(userInput)); // add product to the array list
+
+                System.out.printf("%s'%s' has been added successfully to the shopping cart.%s\n"
+                        , ConsoleColor.BLUE, userInput, ConsoleColor.RESET); // success message
+
+                homeScreen(); // navigate to the home screen
             }
 
-            if (userInput.equalsIgnoreCase("X")) {
-                homeScreen();
+            else
+            {
+                System.out.println(ConsoleColor.RED + "Invalid Input! Please try again."
+                        + ConsoleColor.RESET); // error message
             }
 
-        } while (!isValidOption);
+            if (userInput.equalsIgnoreCase("X"))//
+            {
+                homeScreen(); // navigate to the home screen
+            }
+        }
+        while (!isValidOption);
 
 
     }
 
 
-    // TODO - add user input validation
-// TODO - add try and catch
-    private void displayShoppingCart() {
-
-        distinct = new HashSet<>(shoppingCart);
+    private void displayShoppingCart() //
+    {
+        distinct = new HashSet<>(shoppingCart); // store the unique instances of the arraylist
         isValidOption = false;
-        int productQuantity;
 
-        shoppingCartContent("shopping-cart", 0.00); // display shopping cart content
+        sum = 0.00; // re-initialize the sum
+        sum = shoppingCartContent("shopping-cart", sum); // display shopping cart content
 
-        do {
+        System.out.printf("%7s: %.2f\n", "TOTAL", sum); // display total
+        System.out.println("--------------------------------------------------------------");
+
+        do
+        {
             System.out.println("Would you like to" + ConsoleColor.BLUE_BRIGHT + " check out " + ConsoleColor.RESET + "?");
             System.out.println(ConsoleColor.GREEN + "- 'C' to check out" + ConsoleColor.RESET);
-            System.out.println(ConsoleColor.RED + "- 'X' for home screen" + ConsoleColor.RESET);
+            System.out.println(ConsoleColor.RED_BOLD + "- 'X' for home screen" + ConsoleColor.RESET);
+
             System.out.print("Select a command: ");
             userInput = scanner.nextLine();
 
-            if (userInput.equalsIgnoreCase("C") && (shoppingCart.size() > 0)) {
+            if (userInput.equalsIgnoreCase("C") && (shoppingCart.size() > 0))
+            {
                 checkout();
                 isValidOption = true;
-            } else if (userInput.equalsIgnoreCase("X")) {
+            }
+
+            else if (userInput.equalsIgnoreCase("X"))
+            {
                 homeScreen();
                 isValidOption = true;
-            } else if (userInput.equalsIgnoreCase("C") && shoppingCart.size() == 0) {
-                System.out.println(ConsoleColor.YELLOW + "Unable to checkout! " +
-                        "The shopping cart is empty." + ConsoleColor.RESET);
-            } else {
-                System.out.println(ConsoleColor.RED + "Invalid command! Please try again." + ConsoleColor.RESET);
             }
-        } while (!isValidOption);
 
+            else if (userInput.equalsIgnoreCase("C") && shoppingCart.size() == 0)
+            {
+                System.out.println(ConsoleColor.YELLOW + "Unable to checkout! " +
+                        "The shopping cart is empty." + ConsoleColor.RESET); // warning message
+            }
+
+            else
+            {
+                System.out.println(ConsoleColor.RED + "Invalid command! Please try again."
+                        + ConsoleColor.RESET); // error message
+            }
+        }
+        while (!isValidOption);
     }
 
-    // TODO - add user input validation
-// TODO - add try and catch
-    private void checkout() {
-
-        double sum = 0.00; // declare the variable that would store the total
+    private void checkout() //
+    {
         double cashAmount; // stores the value of the cash amount enter by the user
-        double balance = 0.00; // stores t
+        double balance; // stores the value of the change
 
+        sum = 0.00;
         sum = shoppingCartContent("checkout", sum);
 
         System.out.printf("%7s: %.2f\n", "TOTAL", sum); // display total
@@ -200,41 +240,47 @@ public class OnlineStoreApp {
             cashAmount = scanner.nextDouble(); //store the cash amount of the user
             balance = cashAmount - sum;
 
-            if (balance > 0.00) {
-
-                System.out.printf("Your change is: %.2f\n", balance);
-
-                shoppingCartContent("checkout", sum);
+            if (balance > 0.00)
+            {
+                shoppingCartContent("soldItems", sum);
+                System.out.printf("%sYour change is: $%.2f%s\n", ConsoleColor.BLUE, balance, ConsoleColor.RESET);
                 shoppingCart.clear();
 
                 homeScreen();
-
-            } else {
-                System.out.printf("Cash amount is insufficient\nTotal amount: %.2f", sum);
+            }
+            else
+            {
+                System.out.printf("%sCash amount is insufficient.\n%sTotal amount: %.2f\n", ConsoleColor.YELLOW, ConsoleColor.RESET, sum);
+                homeScreen();
             }
 
-        } catch (InputMismatchException ex) {
-            System.out.println(ConsoleColor.RED + "Invalid input! Please enter a numeric value" + ConsoleColor.RESET);
+        }
+
+        catch (InputMismatchException ex)// catches exception related to the format/data type of the input
+        {
+            System.out.println(ConsoleColor.RED + "Invalid input! Please enter a numeric value." + ConsoleColor.RESET);
             scanner.nextLine();
             checkout();
         }
 
+        catch (Exception ex) // catches any unexpected exception
+        {
+            System.out.println(ConsoleColor.RED + "Something went wrong!" + ConsoleColor.RESET);
+            scanner.nextLine();
+            checkout();
+        }
 
     }
 
-
-    public double shoppingCartContent(String contentFor, double sum) {
-
-        String screenTitle = "";
-        switch (contentFor) {
-
-            case "shopping-cart":
-                screenTitle = "\t\t\t\t\tSHOPPING CART";
-                break;
-            case "checkout":
-                screenTitle = "\t\t\t\t\tONLINE STORE CHECK OUT";
-                break;
-        }
+    public double shoppingCartContent(String contentFor, double sum) //
+    {
+        String screenTitle = switch (contentFor) //
+                {
+                    case "shopping-cart" -> "\t\t\t\t\tSHOPPING CART";
+                    case "checkout" -> "\t\t\t\t\tONLINE STORE CHECK OUT";
+                    case "soldItems" -> "\t\t\t\t\tITEMS SOLD";
+                    default -> "";
+                };
 
         System.out.println("===============================================================");
         System.out.println(screenTitle);
@@ -243,25 +289,21 @@ public class OnlineStoreApp {
         System.out.printf("|%-7s|%-35s|%-7s|%-8s|\n", "ID", "NAME", "PRICE", "QUANTITY");
         System.out.println("------- ----------------------------------- ------ ----------");
 
-        for (Product product : distinct) {
+        for (Product product : distinct) //
+        {
 
-            productQuantity = Collections.frequency(shoppingCart, product);
+            // stores the quantity of the products in the shopping cart
+            int productQuantity = Collections.frequency(shoppingCart, product);
 
             // format and display the content of shopping cart
             System.out.printf("|%7s|%-35s|%7.2f|%8s|\n", product.getId(), product.getName(),
                     product.getPrice(), productQuantity);
             System.out.println("------- ----------------------------------- ------ ----------");
 
-            if (contentFor.equalsIgnoreCase("checkout")) {
-                sum += product.getPrice() * productQuantity;
-
-            }
+            sum += product.getPrice() * productQuantity;
 
         }
-
         return sum;
-
-
     }
 }
 
